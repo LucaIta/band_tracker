ed8c8f1bd2096b9ff49a4217fa5d8e6bc257e4fd
Luca Quatela <lucaqq@gmail.com>
2016-10-01 18:12:53 +0200

    modified database url

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -3,7 +3,7 @@ import org.postgresql.*; // heroku
 
 
 public class DB {
-  public static Sql2o sql2o = new Sql2o("jbdc:postgresql://ysokxizkpzypgx:KRojW1PMJQCqXA4k1MJfXR-9jw@ec2-54-221-229-37.compute-1.amazonaws.com:5432/df5l76otnfghdh", null, null);
+  public static Sql2o sql2o = new Sql2o("postgresql://ysokxizkpzypgx:KRojW1PMJQCqXA4k1MJfXR-9jw@ec2-54-221-229-37.compute-1.amazonaws.com:5432/df5l76otnfghdh", null, null);
   // public static Sql2o sql2o = new Sql2o("postgres://ysokxizkpzypgx:KRojW1PMJQCqXA4k1MJfXR-9jw@ec2-54-221-229-37.compute-1.amazonaws.com:5432/df5l76otnfghdh", null, null);
 
 }
