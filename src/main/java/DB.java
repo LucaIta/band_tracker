a089a4fca94cc9910e56b843be43e16bf16e56bd
Luca Quatela <lucaqq@gmail.com>
2016-10-01 18:29:33 +0200

    modified database url

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -3,8 +3,10 @@ import org.postgresql.*; // heroku
 
 
 public class DB {
-  public static Sql2o sql2o = new Sql2o("postgresql://ysokxizkpzypgx:KRojW1PMJQCqXA4k1MJfXR-9jw@ec2-54-221-229-37.compute-1.amazonaws.com:5432/df5l76otnfghdh", null, null);
-  // public static Sql2o sql2o = new Sql2o("postgres://ysokxizkpzypgx:KRojW1PMJQCqXA4k1MJfXR-9jw@ec2-54-221-229-37.compute-1.amazonaws.com:5432/df5l76otnfghdh", null, null);
+  // public static Sql2o sql2o = new Sql2o("postgresql://ysokxizkpzypgx:KRojW1PMJQCqXA4k1MJfXR-9jw@ec2-54-221-229-37.compute-1.amazonaws.com:5432/df5l76otnfghdh", null, null); // with this URL I get the error "org.sql2o.Sql2oException: Could not acquire a connection from DataSource - The connection attempt failed"
+  // public static Sql2o sql2o = new Sql2o("postgres://ysokxizkpzypgx:KRojW1PMJQCqXA4k1MJfXR-9jw@ec2-54-221-229-37.compute-1.amazonaws.com:5432/df5l76otnfghdh", null, null); // original
+  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-221-229-37.compute-1.amazonaws.com/df5l76otnfghdh", "ysokxizkpzypgx", "KRojW1PMJQCqXA4k1MJfXR-9jw");
+
 
 }
 
