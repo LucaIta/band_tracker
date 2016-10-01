a3432fdea9813a7cd91d37b5131a7691a6d4ebad
Luca Quatela <lucaqq@gmail.com>
2016-10-01 11:11:55 +0200

    change the DB url to try to connect to the Heroku Postgres Server

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,5 @@
 import org.sql2o.*;
 
 public class DB {
-  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/band_tracker", null, null);
+  public static Sql2o sql2o = new Sql2o("postgres://ysokxizkpzypgx:KRojW1PMJQCqXA4k1MJfXR-9jw@ec2-54-221-229-37.compute-1.amazonaws.com:5432/df5l76otnfghdh", null, null);
 }
